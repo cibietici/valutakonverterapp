89a6a50b1c6d485f4ee149bf8205d63bb1399079
carlo alberto burato <carloalberto.burato@gmail.com>
2023-01-27 14:25:22 +0100

    Add JS

---
+++ js/app.js
@@ -0,0 +1,28 @@
+function currencyInit() {
+    const amountElement = document.getElementById('amount');
+    const currencyElement = document.getElementById('currency');
+    const convertButtonElement = document.getElementById('convert');
+    const resultElement = document.getElementById('result');
+
+    convertButtonElement.addEventListener('click', action);
+    window.addEventListener('keyup', (event) => {
+        if(event.key === 'Enter') {
+            action();
+        }
+    });
+
+    function action() {
+        resultElement.textContent = conversion();
+    };
+
+    function conversion() {
+        if(amountElement.value === '') {
+            return 'Please write an amount';
+        }
+        if(currencyElement.value === '') {
+            return 'Please choose a currency';
+        }
+        return Math.floor(amountElement.value * currencyElement.value);
+    };
+}
+currencyInit();
